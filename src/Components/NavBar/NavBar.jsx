55210f1f2350bc076240029d1731754d0a133bb8
ZekaiTombul <zekaitombul@gmail.com>
2025-01-05 04:01:21 +0300

    Create NavBar.jsx

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+import "./NavBar.css";
+export default function Navbar() {
+    return (
+    <div className="navbar">
+        <p>Teknolojik Yemekler</p>
+        <img src="./images/iteration-1-images/home-banner.png" alt="navbar-img" />
+    </div>
+    )
+}
