64f8af5baca59db21673c0577714027fb9300035
ZekaiTombul <zekaitombul@gmail.com>
2025-01-05 05:28:21 +0300

    Create CuisineList.jsx

---
+++ src/Components/Home/CuisineList/CuisineList.jsx
@@ -0,0 +1,19 @@
+import "./CuisineList.css";
+
+
+export default function CuisineList({ cuisines }) {
+
+    return (
+        <div className="cuisine-container">
+            <ul className="cuisine-list">
+                {cuisines.map((cuisine) => (
+                    <li key={cuisine.cuisineName} className="cuisine-item">
+                        <img src={cuisine.img} alt={cuisine.cuisineName} />
+                        {cuisine.cuisineName}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+
+}
