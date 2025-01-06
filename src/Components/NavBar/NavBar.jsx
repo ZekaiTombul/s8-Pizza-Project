9bd97cba04ad7873513044f5fbd947f4c34c8d86
ZekaiTombul <zekaitombul@gmail.com>
2025-01-06 18:42:53 +0300

    Update NavBar.jsx

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,7 +3,6 @@ export default function Navbar() {
     return (
     <div className="navbar">
         <p>Teknolojik Yemekler</p>
-        <img src="./images/iteration-1-images/home-banner.png" alt="navbar-img" />
     </div>
     )
 }
