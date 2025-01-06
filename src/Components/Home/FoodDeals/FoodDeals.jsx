8b8dbd3a2a729f20d75fd4d72cfaf5b86e949e95
ZekaiTombul <zekaitombul@gmail.com>
2025-01-06 18:42:47 +0300

    Update FoodDeals.jsx

--- src/Components/Home/FoodDeals/FoodDeals.jsx
+++ src/Components/Home/FoodDeals/FoodDeals.jsx
@@ -1,4 +1,5 @@
 import "./FoodDeals.css";
+import Button from "../../Button/Button.jsx";
 
 export default function FoodDeals({deals}) {
 
@@ -7,7 +8,33 @@ export default function FoodDeals({deals}) {
             <div style={{
                 backgroundImage : `url(${deals[0].img})`, 
                 backgroundSize:"cover" 
-                }} > 
+                }} 
+                className="top-deal">
+                    <h3>{deals[0].campaign}</h3>
+                    <p>{deals[0].name}</p>
+                    <Button className="deal-button">SİPARİŞ VER</Button>
+            </div>
+            <div className="other-deals">
+                <div className="deal-item"
+                style={{
+                    backgroundImage: `url(${deals[1].img})`,
+                    backgroundSize:"cover"
+                }}
+                >
+                    <h3>{deals[1].campaign}</h3>
+                    <p>{deals[1].name}</p>
+                    <Button className="deal-button">SİPARİŞ VER</Button>
+                </div>
+                <div className="deal-item"
+                style={{
+                    backgroundImage: `url(${deals[2].img})`,
+                    backgroundSize:"cover"
+                }}
+                >
+                    <h3>{deals[2].campaign}</h3>
+                    <p>{deals[2].name}</p>
+                    <Button className="deal-button">SİPARİŞ VER</Button>
+                </div>
             </div>
         </div>
     )
