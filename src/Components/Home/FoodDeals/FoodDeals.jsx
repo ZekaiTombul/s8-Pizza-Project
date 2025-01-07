0ab6d5faefca21d9bbb6f0f9074f3f8e7c5d6418
ZekaiTombul <zekaitombul@gmail.com>
2025-01-07 17:09:27 +0300

    Update FoodDeals.jsx

--- src/Components/Home/FoodDeals/FoodDeals.jsx
+++ src/Components/Home/FoodDeals/FoodDeals.jsx
@@ -1,41 +1,44 @@
 import "./FoodDeals.css";
 import Button from "../../Button/Button.jsx";
 
-export default function FoodDeals({deals}) {
-
+export default function FoodDeals() {
     return (
         <div className="deal-container">
-            <div style={{
-                backgroundImage : `url(${deals[0].img})`, 
-                backgroundSize:"cover" 
-                }} 
-                className="top-deal">
-                    <h3>{deals[0].campaign}</h3>
-                    <p>{deals[0].name}</p>
-                    <Button className="deal-button">SİPARİŞ VER</Button>
+            <div
+                style={{
+                    backgroundImage: `url("../../../../images/iteration-2-images/cta/kart-1.png")`,
+                    backgroundSize: "cover",
+                }}
+                className="top-deal"
+            >
+                <h3>Özel Lezzetus</h3>
+                <p>Position:Absolute Acı Burger</p>
+                <Button className="deal-button">SİPARİŞ VER</Button>
             </div>
             <div className="other-deals">
-                <div className="deal-item"
-                style={{
-                    backgroundImage: `url(${deals[1].img})`,
-                    backgroundSize:"cover"
-                }}
+                <div
+                    className="deal-item"
+                    style={{
+                        backgroundImage: `url("../../../../images/iteration-2-images/cta/kart-2.png")`,
+                        backgroundSize: "cover",
+                    }}
                 >
-                    <h3>{deals[1].campaign}</h3>
-                    <p>{deals[1].name}</p>
+                    <h3>Hackathlon</h3>
+                    <p>Burger Menü</p>
                     <Button className="deal-button">SİPARİŞ VER</Button>
                 </div>
-                <div className="deal-item"
-                style={{
-                    backgroundImage: `url(${deals[2].img})`,
-                    backgroundSize:"cover"
-                }}
+                <div
+                    className="deal-item"
+                    style={{
+                        backgroundImage: `url("../../../../images/iteration-2-images/cta/kart-3.png")`,
+                        backgroundSize: "cover",    
+                    }}
                 >
-                    <h3>{deals[2].campaign}</h3>
-                    <p>{deals[2].name}</p>
+                    <h3 style={{ color: "var(--primary-red)" }}>Çoooook</h3>
+                    <p style={{ color: "var(--primary-black)" }}>hızlı npm gibi kurye</p>
                     <Button className="deal-button">SİPARİŞ VER</Button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
