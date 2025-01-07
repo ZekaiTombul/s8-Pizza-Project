0229cd2d23c1963b93167ea5cccda9fef2cb42f9
ZekaiTombul <zekaitombul@gmail.com>
2025-01-07 17:09:30 +0300

    Update CuisineList.jsx

--- src/Components/Home/CuisineList/CuisineList.jsx
+++ src/Components/Home/CuisineList/CuisineList.jsx
@@ -1,18 +1,24 @@
 import "./CuisineList.css";
 
 
-export default function CuisineList({ cuisines }) {
+export default function CuisineList() {
 
     return (
         <div className="cuisine-container">
+            
             <ul className="cuisine-list">
-                {cuisines.map((cuisine) => (
-                    <li key={cuisine.cuisineName} className="cuisine-item">
-                        <img src={cuisine.img} alt={cuisine.cuisineName} />
-                        {cuisine.cuisineName}
-                    </li>
-                ))}
+                
+                <li className="cuisine-item">
+                        <img src="../../../../images/CuisineList-images/kore.png" alt="kore.png" />
+                        <img src="../../../../images/CuisineList-images/pizza.png" alt="pizza.png" />
+                        <img src="../../../../images/CuisineList-images/burger.png" alt="burger.png" />
+                        <img src="../../../../images/CuisineList-images/kizartmalar.png" alt="kizartmalar.png" />
+                        <img src="../../../../images/CuisineList-images/fastfood.png" alt="fastfood.png" />
+                        <img src="../../../../images/CuisineList-images/icecek.png" alt="icecek.png" />
+                </li>
+               
             </ul>
+        
         </div>
     )
 
