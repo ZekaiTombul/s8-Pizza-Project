098fc1e2942a1bac2803865cb35402baee31f7a2
ZekaiTombul <zekaitombul@gmail.com>
2025-01-08 20:43:47 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Footer from './Components/Footer/Footer'
 import { Route, Switch } from 'react-router-dom'
 import Home from './Components/Home/Home'
 import ErrorPage from './Components/ErrorPage'
+import {pizza} from './Components/data'
+import Success from './Components/Success'
+import Order from './Components/Order/Order'
 
 function App() {
   
@@ -13,13 +16,22 @@ function App() {
     <>
       <Navbar />
       <Switch>
-        <Route path="/" exact><Home /> </Route>
-        <Route path="/order"><Order /> </Route>
-        <Route path="*"><ErrorPage /> </Route>
+        <Route path="/" exact>
+          <Home /> 
+        </Route>
+        <Route path="/order">
+          <Order pizza={pizza} /> 
+        </Route>
+        <Route path="/success">
+          <Success /> 
+        </Route>
+        <Route path="*">
+          <ErrorPage /> 
+        </Route>
       </Switch>
       <Footer />
     </>
-  )
+  );
 }
 
 export default App
