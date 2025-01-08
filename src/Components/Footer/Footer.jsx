00399a6adacd174246db334e4a2dbbc25ad603a0
ZekaiTombul <zekaitombul@gmail.com>
2025-01-08 20:43:39 +0300

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,20 +6,20 @@ export default function Footer(){
             <div className="footer-contact">
                 <h2>Teknolojik Yemekler</h2>
                 <p>
-                    <img src="#" alt="konum icon" />
+                    <img src="../../../../images/iteration-2-images/footer/icons/icon-1.png" alt="konum icon" />
                     341 Londonderry Road, Istanbul Türkiye
                 </p>
                 <p>
-                    <img src="#" alt="mail icon" />
+                    <img src="../../../../images/iteration-2-images/footer/icons/icon-2.png" alt="mail icon" />
                     aciktim@teknolojikyemekler.com
                 </p>
                 <p>
-                    <img src="#" alt="telefon icon" />
+                    <img src="../../../../images/iteration-2-images/footer/icons/icon-3.png" alt="telefon icon" />
                     +90 216 123 45 67
                 </p>
             </div>
             <div className="footer-quick-menu">
-                <h4>
+                <h4> Hot Menu
                     <p>Terminal Pizza</p>
                     <p>5 Kişilik Hackathlon Pizza</p>
                     <p>useEffect Tavuklu Pizza</p>
@@ -30,6 +30,12 @@ export default function Footer(){
             </div>
             <div>
                 <h4>Instagram</h4>
+                <img src="../../../../images/iteration-2-images/footer/insta/li-0.png"/>
+                <img src="../../../../images/iteration-2-images/footer/insta/li-1.png"/>
+                <img src="../../../../images/iteration-2-images/footer/insta/li-2.png"/>
+                <img src="../../../../images/iteration-2-images/footer/insta/li-3.png"/>
+                <img src="../../../../images/iteration-2-images/footer/insta/li-4.png"/>
+                <img src="../../../../images/iteration-2-images/footer/insta/li-5.png"/>
             </div>
         </div>
     )
